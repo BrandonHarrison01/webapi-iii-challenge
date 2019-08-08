ba01418defd6c61f590c7fc98562a661e6d4e6cb
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-08-08 14:00:16 -0600

    made port dynamic for heroku

--- index.js
+++ index.js
@@ -21,5 +21,6 @@ server.get('/', (req, res) => {
     res.status(200).json({ api: 'api is running...' })
 })
 
+const port = process.env.PORT || 3333;
 
-server.listen(3333, () => console.log('api is running on port 3333'))
+server.listen(port, () => console.log(`api is running on port ${port}`))
